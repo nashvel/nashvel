31f899723d1b8d7fd6c524ca6d7646762160099d
nacht <nash.backup21@gmail.com>
2025-04-03 10:44:02 +0800

    Create SignUp.tsx

---
+++ SignUp.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+export default function SignUp() {
+  const [form, setForm] = useState({ email: "", password: "" });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("Signing up:", form);
+  };
+
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSubmit}>
+        <input name="email" type="email" placeholder="Email" onChange={handleChange} required />
+        <input name="password" type="password" placeholder="Password" onChange={handleChange} required />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+}
